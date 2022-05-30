fa979bbc073970c35dcefaecd675c7c17a2c66b0
Joseph <57100116+AbsoluteZer03@users.noreply.github.com>
2022-05-30 03:17:16 -0400

    Finished extension

--- Day 2/extensions.py
+++ Day 2/extensions.py
@@ -1,16 +1,12 @@
 #File Extensions
 
-
 def main():
-    list = [".gif", ".jpg", ".jpeg", ".png",".pdf", ".txt", ".zip"]
-    check = str
-
-    file = input("What is the name of your file.")
-    file = file.lower()
-    if file in list:
-        for i in range(7):
-            if file in list[i]:
-                
-
-
+    list1 = {".gif":"image/gif", ".jpg":"image/jpg", ".jpeg": "image/jpeg", ".png":"image/png",".pdf":"application/pdf",
+     ".txt":"text/plain", ".zip":"application/zip"}
+    file = input("What is the name of your file. \n").lower().split(".", 1)[1]
+    file = ("." + file)
+    if file in list1:
+        print(list1[file])
+    else:
+        print("application/octet-stream")
 main()
